9212a81f7f5e1f5f67f6f214c4ea9fe07bcd7932
Movinghead333 <Movinghead333@googlemail.com>
2020-02-09 20:13:23 +0100

    initial commit

---
+++ AStarPathing/Assets/AStarPathfinding.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarPathfinding : MonoBehaviour
+{
+    public Pixel p;
+    public GameObject pixel;
+    public Canvas can;
+    int width = 8;
+    int height = 8;
+
+    public GameObject[,] pixels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform t = gameObject.transform;
+        Vector3 offset = new Vector3(-400, -400);
+
+        pixels = new GameObject[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                t.position = offset + new Vector3(x * 110, y * 110, 0);
+                pixels[x, y] = Instantiate(pixel, t.position, Quaternion.identity);
+                pixels[x, y].transform.SetParent(can.transform, false);
+            }
+        }
+
+        //Transform t = gameObject.transform;
+        //t.position = new Vector3(100, 100, 0);
+        //GameObject g = Instantiate(pixel, t.position, Quaternion.identity);
+        //g.transform.SetParent(can.transform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AStarPathing/Assets/Pixel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Pixel : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI currentPathLengthText;
+    [SerializeField]
+    TextMeshProUGUI estimatedPathLengthText;
+    [SerializeField]
+    TextMeshProUGUI estimatedPathLength2Text;
+    [SerializeField]
+    TextMeshProUGUI minimumPathLengthText;
+
+    public bool isObstacle = false;
+
+    void setCurrent(double current)
+    {
+        currentPathLengthText.SetText(current.ToString());
+    }
+
+    void setEstimated(double estimated)
+    {
+        estimatedPathLengthText.SetText(estimated.ToString());
+    }
+
+    void setEstimated2(double estimated2)
+    {
+        estimatedPathLength2Text.SetText(estimated2.ToString());
+    }
+
+    void setMinimum(double minimum)
+    {
+        minimumPathLengthText.SetText(minimum.ToString());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setMinimum(90.34);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
